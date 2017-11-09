f30f035de8d9f80b637c81c42143c8257043e0ba
sedhumadhavan <sedhumadhavanit@gmail.com>
2017-11-09 19:04:57 +0530

    about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :employees
   namespace :visitors do
   	get :index
+  	get :about
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
