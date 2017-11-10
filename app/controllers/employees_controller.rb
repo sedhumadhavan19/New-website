3c277d8070ad7da3f32cbec217726d7fccc16675
sedhumadhavan <sedhumadhavanit@gmail.com>
2017-11-10 11:59:13 +0530

    papaerclip field added

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -69,6 +69,6 @@ class EmployeesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def employee_params
-      params.require(:employee).permit(:name, :designation)
+      params.require(:employee).permit(:name, :designation, :avatar)
     end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,7 @@
 class Employee < ApplicationRecord
 	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+	
+	#Enum
+  enum designation: [:Front_End, :Back_End, :Full_Stack]
 end
